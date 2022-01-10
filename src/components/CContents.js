591b5e8c2498d9ac21d711e4e5de09b127166a5d
Kacper Kawula <kacperbw@gmail.com>
2022-01-10 07:28:49 +0100

    1

---
+++ src/components/CContents.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+
+class CContents extends Component {
+    
+    constructor(){
+        super()
+        this.state = {msg: 'Witaj użytkowniku. Kliknij przycisk aby poznać wiadomość'}
+
+    }
+
+    changeMsg(){
+        this.setState({msg: 'Własnie jestes uczestnikiem kursu React, gratulacje!'})
+    }
+
+
+    render(){
+        return (
+            <div>
+                <h1>{this.state.msg}</h1>
+                <button onClick={() => this.changeMsg()}>Kliknij, aby poznać tajemnicę</button>
+            </div>
+        )
+    }
+
+}
+
+export default CContents
---
+++ src/components/CFooter.js
@@ -0,0 +1,42 @@
+import React from "react";
+import './CFooter.css';
+
+
+// function CFooter() {
+//   let date = new Date();
+//   return (
+//     <div>
+//       <h5>
+//         PPFront-end, przykladowy serwis React. Dzisiaj mamy:{" "}
+//         {date.toDateString()}{" "}
+//       </h5>
+//     </div>
+//   );
+// }
+
+
+export const CFooter = (props) => {
+    let date = new Date();
+    return (
+      <div>
+        <h5>
+          PPFront-end, przykladowy serwis React. Dzisiaj mamy: {date.toDateString()}
+          <br/>
+          Stan pogody: {props.weather}, temperatura: {props.temp}.
+          <br/>
+          {props.children}
+        </h5>
+      </div>
+    );
+  }
+  
+
+// export const CFooter = () => {
+//   let date = new Date();
+//   return React.createElement('div',{className: 'footerDivClass'},
+//   React.createElement('h5', {id: 'footerText', className: 'footerTextClass'},
+//   'PPFront-end, przykladowy serwis React. Dzisiaj mamy: ' +date.toDateString()));
+// }
+
+//export default CFooter;
+ 
