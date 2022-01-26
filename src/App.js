2a520f9e0c43980038ba91f70f6f41d6d81fb9f8
Kacper Kawula <kacperbw@gmail.com>
2022-01-26 23:48:25 +0100

    1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import {CFooter} from './components/CFooter'
 import CHeader from './components/CHeader';
-import CContents from './components/CContents'
+// import CContents from './components/CContents'
+import COuter from './components/COuter';
 
 function App() {
   return (
     <div className="App">
       <CHeader aName="Jan" aFamily="Kowalski"/>
       {/* <CHeader aName="Tomasz" aFamily="Nowak"/> */}
-      <CContents/>
+      {/* <CContents/> */}
+      <COuter/>
+
       <CFooter weather="słoneczna" temp="25">
       <p> Niebo jest bezchmurne, wiatr słaby.</p>
       </CFooter>
--- src/components/CContents.js
+++ src/components/CContents.js
@@ -5,10 +5,11 @@ class CContents extends Component {
     constructor(){
         super()
         this.state = {msg: 'Witaj użytkowniku. Kliknij przycisk aby poznać wiadomość'}
-
+        console.log('Przycisk został wciśnięty.');
+      // this.changeMsg = this.changeMsg.bind(this);
     }
 
-    changeMsg(){
+    changeMsg = () => {
         this.setState({msg: 'Własnie jestes uczestnikiem kursu React, gratulacje!'})
     }
 
@@ -17,7 +18,7 @@ class CContents extends Component {
         return (
             <div>
                 <h1>{this.state.msg}</h1>
-                <button onClick={() => this.changeMsg()}>Kliknij, aby poznać tajemnicę</button>
+                <button onClick={this.changeMsg}>Kliknij, aby poznać tajemnicę</button>
             </div>
         )
     }
